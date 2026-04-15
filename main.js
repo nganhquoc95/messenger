34b942faab6e7e636952c1e43d8e9e6fcfe322bc
Nguyen Anh Quoc <nganhquoc95@gmail.com>
2026-04-15 18:10:56 +0700

    Add auto-updater and splash screen

--- main.js
+++ main.js
@@ -12,6 +12,33 @@ const { updateBadge } = require('./src/badge');
 const { openFacebookHandler } = require('./src/handlers/open-facebook-handler');
 const { getUnreadMessageCounter } = require('./src/handlers/get-unread-message-counter');
 const styleMessages = require('./src/handlers/styles/messages');
+const { autoUpdater } = require('electron-updater');
+
+// Configure autoUpdater
+autoUpdater.autoDownload = true;
+autoUpdater.allowPrerelease = false;
+
+autoUpdater.on('checking-for-update', () => {
+    console.log('Checking for update...');
+});
+autoUpdater.on('update-available', (info) => {
+    console.log('Update available:', info);
+});
+autoUpdater.on('update-not-available', (info) => {
+    console.log('Update not available:', info);
+});
+autoUpdater.on('error', (err) => {
+    console.log('Error in auto-updater:', err);
+});
+autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ")";
+    console.log(log_message);
+});
+autoUpdater.on('update-downloaded', (info) => {
+    console.log('Update downloaded');
+});
 
 let win;
 
@@ -58,6 +85,9 @@ app.whenReady().then(() => {
             splash.destroy();
             win.show();
         }
+
+        // Check for updates
+        autoUpdater.checkForUpdatesAndNotify();
     });
 
     win.on('close', (event) => {
